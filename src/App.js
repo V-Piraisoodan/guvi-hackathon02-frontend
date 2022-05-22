01102d695c5b4d36007d9d21b0f40f8902d82ad6
V-Piraisoodan <piraisoodanv1996@gmail.com>
2022-05-22 14:08:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const pizza_items = [{
+    "name": "Scone Pizza Recipe",
+    "pic" : "https://i.ndtvimg.com/i/2016-04/crusty-gourmet-pizzas-gurgaon-625_625x..."
+    },{
+      "name" : "Multigrain Pizza Recipe",
+      "pic" : "https://i.ndtvimg.com/i/2015-02/pizza_625x350_51424681477.jpg"
+    },{
+    "name" : "Chicken Pizza Recipe",
+    "pic" : "https://i.ndtvimg.com/i/2015-02/meat-pizza_625x350_41424681507.jpg"
+    },{
+    "name" : "Margherita Pizza Recipe",
+    "pic" : "https://i.ndtvimg.com/i/2015-02/cheese-pizza_625x350_71424681540.jpg"
+    }];
+
+
 
 export default App;
